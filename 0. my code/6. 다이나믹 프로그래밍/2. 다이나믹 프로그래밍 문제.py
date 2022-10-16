4be36b3d331a999373a79cedca2fe2bc4c8f80b3
yehuinkim <92871811+yehuinkim@users.noreply.github.com>
2022-10-16 17:12:19 +0900

    Update and rename 2. 다이나믹 프로그래밍 문제 to 2. 다이나믹 프로그래밍 문제.py

--- 0. my code/6. 다이나믹 프로그래밍/2. 다이나믹 프로그래밍 문제.py
+++ 0. my code/6. 다이나믹 프로그래밍/2. 다이나믹 프로그래밍 문제.py
@@ -34,5 +34,20 @@ for i in range(3,n+1):
 print(d[n])
 
 문제4. 효율적인 화폐구성
+n,m = map(int, input().split())
+array=[]
+for i in range(n):
+  array.append(int(input()))
 
+d=[10001]*(m+1)
+d[0]=0
+for i in range(n):
+  for j in range(array[i], m+1):
+    if d[j-array[i]] !=1001:
+      d[j]=min(d[j], d[j-array[i]]+1)
+
+if d[m] == 10001:
+  print(-1)
+else:
+  print(d[m])
 
