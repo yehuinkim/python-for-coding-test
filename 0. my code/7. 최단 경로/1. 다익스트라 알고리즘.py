c4e5eddc4d0f1c18d4db3e8a9866a46a8cd122ad
yehuinkim <92871811+yehuinkim@users.noreply.github.com>
2022-10-17 17:41:38 +0900

    Update 1. 다익스트라 알고리즘.py

--- 0. my code/7. 최단 경로/1. 다익스트라 알고리즘.py
+++ 0. my code/7. 최단 경로/1. 다익스트라 알고리즘.py
@@ -23,11 +23,13 @@
 
 import heapq
 
-def heapsort(iterable):
+def heapsort(iterable): # 오름차순 힙 정렬
   h = []
   result = []
+  # 모든 원소 차례대로 힙에 삽입
   for value in iterable:
     heapq.heappush(h, value)
+  # 힙에 삽입된 모든 원소를 차례대로 꺼내서 result에 담기
   for i in range(len(h)):
     result.append(heapq.heappop(h))
   return result
