def79441fc1e8db8fd35b26c53af601ac8721fc2
yehuinkim <92871811+yehuinkim@users.noreply.github.com>
2022-10-17 19:31:02 +0900

    Update 3. 최단경로 기초문제.py

--- 0. my code/7. 최단 경로/3. 최단경로 기초문제.py
+++ 0. my code/7. 최단 경로/3. 최단경로 기초문제.py
@@ -32,3 +32,40 @@ else:
   print(distance)
 -----------------------------------------------------------------------------
 문제2. 전보
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n,m,start = map(int, input().split())
+graph =[[] for i in range(n+1)]
+distance = [INF]*(n+1)
+
+for _ in range(m):
+  x,y,z = map(int, input().split())
+  graph[x].append((y,z))
+
+def dijkstra(start):
+  q = []
+  heapq.heappush(q, (0,start))
+  distance[start]=0
+  while q:
+    dis, now = heapq.heappop(q)
+    if distance[now]< dis:
+      continue
+    for i in graph[now]:
+      cost = dis +i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(cost, i[0])
+
+dijkstra(start)
+count = 0
+max_distance = 0
+for i in distance:
+  if i != INF:
+    count += 1
+    max_distance = max(i, max_distance)
+
+print(count-1, max_distance)
